6496e5f5be4d286182236cc59817bb53f223597e
joshua-murphy <josh.murphy11@gmail.com>
2017-11-29 16:40:05 -0700

    controller and model built with basic functions

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,13 +1,45 @@
 class BillboardsController < ApplicationController
+
+  before_action :billboard_set, only: [:show, :edit, :update, :destroy]
+  # before_action :billboard_params
+
+  def index
+    @billboards = Billboard.all
+  end
+  
+  def show
+  end
+  
   def new
+    @billboard = Billboard.new
   end
 
-  def show
+  def create
+    @billboard = Billboard.new(billboard_params)
+    @billboard.save ? (redirect_to billboard_path(@billboard)) : (render :new)
   end
 
   def edit
   end
 
-  def index
+  def update
+    @billboard.update(billboard_params)   
+    @billboard.save ? (redirect_to billboard_path(@billboard)) : (render :edit)    
   end
+
+  def destroy
+    @billboard.destroy
+    redirect_to billboards_path
+  end
+
+  private
+
+    def billboard_set
+      @billboard = Billboard.find(params[:id])
+    end
+
+    def billboard_params
+      params.require(:billboard).permit(:name, :count, :genre)
+    end
+
 end
--- db/migrate/20171129214945_create_billboards.rb
+++ db/migrate/20171129214945_create_billboards.rb
@@ -1,9 +1,9 @@
 class CreateBillboards < ActiveRecord::Migration[5.1]
   def change
     create_table :billboards do |t|
-      t.string :name
-      t.integer :count
-      t.string :genre
+      t.string :name, null: false
+      t.integer :count, null:false, default: 100
+      t.string :genre, null:false
 
       t.timestamps
     end
